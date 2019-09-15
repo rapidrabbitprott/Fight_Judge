7d770efe02080017d2be8086bb0f45f4e14dbf4a
Masataka Morikawa <morikawamasataka@keio.jp>
2019-09-15 12:15:56 +0900

    Initial Commit

---
+++ Fight Judge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fight Judge
+//
+//  Created by 森川正崇 on 2019/09/15.
+//  Copyright © 2019 morikawamasataka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
